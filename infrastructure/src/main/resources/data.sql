120fb94c8d62d1ca2da023d806bdd653d2238950
reneald <delea.vanreeth@gmail.com>
2018-04-26 12:02:43 +0200

    added data.sql

---
+++ infrastructure/src/main/resources/data.sql
@@ -0,0 +1,35 @@
+INSERT INTO items (id, name, description, amount_of_stock, price_amount)
+    VALUES (
+        '840d3472-4935-11e8-842f-0ed5f89f718b',
+        'TestItem1',
+        'TestDescription1',
+        50,
+        20
+    );
+    
+    INSERT INTO items (id, name, description, amount_of_stock, price_amount)
+    VALUES (
+        '840d36fc-4935-11e8-842f-0ed5f89f718b',
+        'TestItem2',
+        'TestDescription2',
+        50,
+        20
+    );
+    
+    INSERT INTO items (id, name, description, amount_of_stock, price_amount)
+    VALUES (
+        '840d397c-4935-11e8-842f-0ed5f89f718b',
+        'TestItem3',
+        'TestDescription3',
+        55,
+        25
+    );
+    
+    INSERT INTO items (id, name, description, amount_of_stock, price_amount)
+    VALUES (
+        '840d3ab2-4935-11e8-842f-0ed5f89f718b',
+        'TestItem4',
+        'TestDescription4',
+        60,
+        30
+    );
